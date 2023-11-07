1f3ffbdc749b3e8ec8de2ba6ddf51f3c7c399074
Dmytro <zhorov.dmytro@gmail.com>
2023-11-07 22:19:28 +0200

    1 and 3 lesson

--- HW_1_7_1/Program.cs
+++ HW_1_7_1/Program.cs
@@ -1,1 +1,80 @@
-﻿
+﻿//Використовуючи Visual Studio, створіть проект за шаблоном Console Application. Створіть чотири методи до виконання арифметичних операцій,
+//з іменами: Add – додавання, Sub – віднімання, Mul – множення, Div – поділ. Кожен метод повинен приймати два цілих аргументи і виводити на екран результат
+//виконання арифметичної операції відповідної імені методу. Метод поділу Div повинен виконувати перевірку спроби поділу на нуль.
+//Потрібно надати користувачеві можливість вводити з клавіатури значення операндів та знак арифметичної операції для виконання обчислень.
+
+Console.WriteLine("Enter the first number ");
+var input1 = Console.ReadLine();
+int.TryParse(input1, out int operand1);
+
+Console.WriteLine("Enter the second number ");  
+var input2 = Console.ReadLine();
+int.TryParse(input2, out int operand2);
+
+Console.WriteLine("Enter arithmetic operation '+' '-' '*' '/': ");
+char sing = Convert.ToChar(Console.ReadLine());
+
+int result;
+
+switch (sing)
+{
+    case '+':
+    {
+        result = Add(operand1, operand2);
+        Console.WriteLine($"Your answer {result}");
+        break;
+    }
+    case '-':
+    {
+        result = Sub(operand1, operand2);
+        Console.WriteLine($"Your answer {result}");
+        break;
+    }
+    case '*':
+    {
+        result = Nul(operand1, operand2);
+        Console.WriteLine($"Your answer {result}");
+        break;
+    }
+    case '/':
+    {
+        if (operand2 != 0)
+        {
+            result = Div(operand1, operand2);
+            Console.WriteLine($"Your answer {result}");
+        }
+        else
+        {
+            Console.WriteLine($"You cannot divide by {operand2}");
+        }
+        break;
+    }
+    default:
+    {
+        Console.WriteLine("You entered an incorrect arithmetic operation");
+        break;
+    }
+}
+
+//Console.WriteLine($"Your answer {result}");
+
+static int Add(int operand1, int operand2)
+{
+    return operand1 + operand2;
+}
+
+static int Sub(int operand1, int operand2)
+{
+    return operand1 - operand2;
+}
+
+static int Nul(int operand1, int operand2)
+{
+    return operand1 * operand2;
+}
+
+static int Div(int operand1, int operand2)
+{
+    return operand1 / operand2;
+}
+
---
+++ HW_1_7_3/Program.cs
@@ -0,0 +1,74 @@
+﻿//Використовуючи Visual Studio, створіть проект за шаблоном Console Application.
+//Напишіть метод, який визначатиме:
+//1) чи є введене число позитивним чи негативним.
+//2) Чи є воно простим (використовуйте техніку перебору значень). (Просте число - це натуральне число,
+//яке ділиться на 1 і саме на себе. Щоб визначити просте число чи ні,слід знайти всі цілі дільники.
+//Якщо дільників більше 2-х, значить воно не просте.)
+//3) Чи ділиться на 2 , 5, 3, 6, 9 без залишку.
+//-------------------------------------------------------------------------------------------------------
+
+
+internal class HW_1_7_3
+{
+    static string DefinitionSign(int number)
+    {
+        if (number > 0)
+        {
+            return "The number is positiv";
+        }
+        else if (number == 0)
+        {
+            return "The number is neither positive nor negative";
+        }
+        else
+        {
+            return "The number is negative";
+        }
+    }
+
+    static string PrimeNumber(int number)
+    {
+        if (number <= 1)
+        {
+            return "The number is not prime";
+        }
+
+        for (int i = 2; i <= Math.Sqrt(number); i++)
+        {
+            if (number % i == 0)
+            {
+                return "The number is not prime"; 
+            }
+        }
+        
+        return "The number is  prime";
+
+    }
+
+    static string Division(int number)
+    {
+        if (number % 2 == 0 && number % 5 == 0 && number % 3 == 0 && number % 6 == 0 && number % 9 == 0)
+        {
+            return "Shares without a remainder";
+        }
+        
+        return "Shares with a remainder";
+    }
+    
+    
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter number ");
+        var input1 = Console.ReadLine();
+        int.TryParse(input1, out int number);
+        
+        string definiti = DefinitionSign(number);
+        Console.WriteLine(definiti);
+        
+        string prime = PrimeNumber(number);
+        Console.WriteLine(prime);
+        
+        string division = Division(number);
+        Console.WriteLine(division);
+    }
+}
