0a5a2c4ce9383caa58646e08daae39ee8a55e8a0
Dmytro <zhorov.dmytro@gmail.com>
2023-11-12 14:37:12 +0200

    4 leson

---
+++ HW_1_7_4/Program.cs
@@ -0,0 +1,25 @@
+﻿//Використовуючи Visual Studio, створіть проект за шаблоном Console Application.
+//Створіть метод з ім'ям Calculate, який приймає як параметри три цілих аргументи і виводить на екран середнє арифметичне значень аргументів.
+
+Console.WriteLine("Hello, World!");
+
+
+Console.WriteLine("Enter the first number ");
+var input1 = Console.ReadLine();
+int.TryParse(input1, out int operand1);
+
+Console.WriteLine("Enter the second number ");  
+var input2 = Console.ReadLine();
+int.TryParse(input2, out int operand2);
+
+Console.WriteLine("Enter the third number ");  
+var input3 = Console.ReadLine();
+int.TryParse(input3, out int operand3);
+
+double avarageValue = AvarageValue(operand1, operand2, operand3);
+Console.WriteLine($"Avarage value is {avarageValue}");
+
+static double AvarageValue(int operand1, int operand2, int operand3)
+{
+    return (operand1 + operand2 + operand3) / 3;
+}
